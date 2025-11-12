157258ece119e49d96ca0a5630562e679b4fd175
Mykola Vusotski <mykolavusotski@MacBook-Air-Mykola.local>
2025-11-12 12:51:51 +0200

    add changes

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log("Hello world!");
+// index.js
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+  // завантажуємо dist/index.html
+  const filePath = path.join(__dirname, 'dist', 'index.html');
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      res.writeHead(500);
+      res.end('Помилка сервера');
+      return;
+    }
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end(data);
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`Сервер запущено на http://localhost:${PORT}`);
+});
